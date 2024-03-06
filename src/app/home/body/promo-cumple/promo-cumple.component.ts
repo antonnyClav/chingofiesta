2595657a3937d4246d93118d09bff7d83a7b0332
Antx <anto_clav@hotmail.com>
2024-03-06 16:22:17 -0300

    solapa cumpleaños

--- src/app/home/body/promo-cumple/promo-cumple.component.ts
+++ src/app/home/body/promo-cumple/promo-cumple.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { PublicacionExt } from 'src/app/shared/models/Publicacion';
+import { PublicacionesService } from 'src/app/shared/services/Publicaciones.service';
 @Component({
   selector: 'app-promo-cumple',
   templateUrl: './promo-cumple.component.html',
   styleUrls: ['./promo-cumple.component.css']
 })
 export class PromoCumpleComponent {
+  constructor(private router: Router, private publicacionesService: PublicacionesService) {
+    this.getPromoCumple();
+  } 
+  promosCumple: PublicacionExt[] = [];
+
+  getPromoCumple() {
+    this.publicacionesService.getPromoCumple()
+    /*.pipe(
+      map( response => response )
+    )*/
+    .subscribe( data => { 
+      //console.log(data);
+      this.promosCumple = data;
+    });
+  }
 
 }
